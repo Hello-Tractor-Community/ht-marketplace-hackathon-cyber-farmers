2448cefc1b594c37a94bd1578ee8befcf2e63591
wathika-eng <wathika02@gmail.com>
2024-11-22 21:50:38 +0300

    Resolved merge conflicts with master

--- cyber-farmers/app/page.tsx
+++ cyber-farmers/app/page.tsx
@@ -1,17 +1,18 @@
-"use client";
+'use client';
 
 import { CldImage } from 'next-cloudinary';
 
 export default function Home() {
+  console.log('Home');
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Cloudinary Image in Next.js with CldImage</h1>
       <CldImage
-        src="media/cyberFarmers/8943ba0de16ed09560b83289fd89aa89.jpg"
-        width="500"
-        height="500"
-        alt="Cyber Farmers Image"
-        crop="scale"
+        src='media/cyberFarmers/8943ba0de16ed09560b83289fd89aa89.jpg'
+        width='500'
+        height='500'
+        alt='Cyber Farmers Image'
+        crop='scale'
         priority
       />
     </div>
